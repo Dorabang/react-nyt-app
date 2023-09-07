4e82ce87852c3bd98f4b0a115ffa38e540a866e9
Dorabang <psa1195@naver.com>
2023-09-08 03:46:57 +0900

    feat: 필터 옵션 객체를 담은 배열 생성

---
+++ src/constants/FilterOptions.ts
@@ -0,0 +1,10 @@
+import SearchIcon from 'assets/Search.png';
+import DateIcon from 'assets/active/Date.png';
+
+const FilterOptions = [
+  { id: 0, name: '전체 헤드라인', icon: SearchIcon },
+  { id: 1, name: '전체 날짜', icon: DateIcon },
+  { id: 2, name: '전체 국가' },
+];
+
+export default FilterOptions;
