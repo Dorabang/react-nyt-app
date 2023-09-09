9640be18db37cf05fb59307db77fd6d17fbc8bed
Dorabang <psa1195@naver.com>
2023-09-09 22:31:38 +0900

    feat: REST API를 이용해 데이터 호출 함수(진행 중)

--- src/api/getPost.ts
+++ src/api/getPost.ts
@@ -2,11 +2,15 @@ import axios from 'libs/axios';
 
 type DataType = any;
 
-export const getPost = async (q: string, fq?: string): Promise<DataType[]> => {
+export const getPost = async (
+  q: string,
+  period?: string,
+  glocations?: string
+): Promise<DataType> => {
   const response = await axios.get(
-    `?${q ? `q=${q}&` : ''}${fq ? `fq=${fq}&` : ''}api-key=${
-      process.env.REACT_APP_API_KEY
-    }`
+    `?${q ? `q=${q}&` : ''}${period && glocations ? `fq=` : ''}${
+      period ? `${period}&` : ''
+    }api-key=${process.env.REACT_APP_API_KEY}`
   );
   return response.data.response.docs;
 };
