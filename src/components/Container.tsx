007846aa726d588ece7fee27c08f824989d3362f
Dorabang <psa1195@naver.com>
2023-09-09 22:32:18 +0900

    design: 등록한 색상 값으로 변경, CSS 추가

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='border-t border-[#C4C4C4] bg-gray-bg h-full flex justify-center items-center'>
-      {children}
+    <div className='flex-grow pb-[85px] bg-gray-bg border-t border-gray-font overflow-y-scroll scrollbar-hide'>
+      <div className='h-full flex justify-center items-center'>{children}</div>
     </div>
   );
 };
