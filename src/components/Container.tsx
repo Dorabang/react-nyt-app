5bfa1842377a4f68585bfd05ec23f14030967e02
Dorabang <psa1195@naver.com>
2023-09-10 20:49:37 +0900

    design: CSS 수정

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Container = ({ children }: { children: React.ReactNode }) => {
+const Container = ({
+  onScroll,
+  children,
+}: {
+  onScroll?: (e: any) => void;
+  children: React.ReactNode;
+}) => {
   return (
-    <div className='flex-grow pb-[85px] bg-gray-bg border-t border-gray-font overflow-y-scroll scrollbar-hide'>
-      <div className='h-full flex justify-center items-center'>{children}</div>
+    <div
+      onScroll={(e) => onScroll && onScroll(e)}
+      className='flex-grow pb-[85px] bg-gray-bg border-t border-gray-font overflow-y-scroll scrollbar-hide'
+    >
+      {children}
     </div>
   );
 };
