8325732ba6e2116c7c34070340212db75c7fc89d
Dorabang <psa1195@naver.com>
2023-09-20 14:30:25 +0900

    fix: API 주소 리턴 함수 오타 수정 및 glocations가 undefined일 경우에도 요청되는 에러 수정

--- src/libs/getAPIURL.ts
+++ src/libs/getAPIURL.ts
@@ -7,11 +7,17 @@ const getAPIURL = (
   const glocationsFormat =
     glocations && glocations?.map((value) => `"${value}"`);
 
-  const url = `?${query || period || glocationsFormat ? 'fq=' : ''}${
+  const url = `?${query || period || glocations?.length !== 0 ? 'fq=' : ''}${
     query ? `headline:("${query}")` : ''
   }${query && period ? ' AND ' : ''}${period ? `pub_date:("${period}")` : ''}${
-    period && glocations?.length !== 0 ? ' AND ' : ''
-  }${glocationsFormat ? `glcations:(${glocationsFormat?.toString()})` : ''}${
+    (period && glocations?.length !== 0) || (query && glocations?.length !== 0)
+      ? ' AND '
+      : ''
+  }${
+    glocations?.length !== 0
+      ? `glocations:(${glocationsFormat?.toString()})`
+      : ''
+  }${
     query || period || glocations ? '&' : ''
   }page=${page}&sort=newest&api-key=${process.env.REACT_APP_API_KEY}`;
   return url;
